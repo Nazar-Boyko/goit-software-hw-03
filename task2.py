ab97fca6df94316c1bf7f9e7e4d8519cd99bd64a
Nazar Boyko <nazar.bojko.2004@gmail.com>
2026-06-26 14:57:56 +0300

    fix task2

--- task2.py
+++ task2.py
@@ -2,67 +2,49 @@ from time import time
 from multiprocessing import Pool, Process, cpu_count
 
 
-
-# def factorize(*number):
-
-    
-#     all_divisors = []
-#     for n in number:
-#         all_divisors.append(factorize_one(n))
-#     return all_divisors
-
-def factorize_one(number):
+def get_divisors(number):
     divisors = []
-
-    for i in range(1, int(number**0.5)+1):
+    for i in range(1, number + 1):
         if number % i == 0:
             divisors.append(i)
 
     return divisors
 
 
+def factorize_sync(*numbers):
+    
+    divisors = []
+    for number in numbers:
+        divisors.append(get_divisors(number))
+    
+    return divisors
+
+
+def factorize_parallel(*numbers):
+
+    num_cores = cpu_count()
+    with Pool(processes=num_cores) as pool:
+        divisors = pool.map(get_divisors,numbers)
+
+    return divisors
+
 if __name__ == "__main__":
+    test_data = (128, 255, 99999, 10651060)
 
-    numbers = [
-    99999937,
-    99999935,
+    start_time = time()
+    a_s, b_s, c_s, d_s = factorize_sync(*test_data)
+    sync_duration = time() - start_time
+    print(f"Синхронне виконання зайняло: {sync_duration:.4f} секунд")
+
+    start_time = time()
+    a, b, c, d = factorize_parallel(*test_data)
+    parallel_duration = time() - start_time
+    print(f"Паралельне виконання зайняло: {parallel_duration:.4f} секунд")
+
+    assert a == [1, 2, 4, 8, 16, 32, 64, 128]
+    assert b == [1, 3, 5, 15, 17, 51, 85, 255]
+    assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
+    assert d == [1,2,4,5,7,10,14,20,28,35,70,140,76079,152158,304316,380395,532553,760790,1065106,1521580,2130212,2662765,5325530, 10651060,
     ]
 
-
-    start1 = time()
-
-    result1 = [
-        factorize_one(n)
-        for n in numbers
-    ]
-
-    end1 = time()
-
-
-    print("Sync:")
-
-    for res in result1:
-        print(res)
-
-    print(end1 - start1)
-
-
-
-    with Pool(processes=cpu_count()) as pool:
-
-        start2 = time()
-
-        result2 = pool.map(
-            factorize_one,
-            numbers
-        )
-
-        end2 = time()
-
-
-    print("Parallel:")
-
-    for res in result2:
-        print(res)
-
-    print(end2 - start2)
+    print("\n[Успіх!] Всі assert-перевірки пройдено успішно.")
